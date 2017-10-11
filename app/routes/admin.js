d058f420d252c69bac91435417104fc332ecc1f9
George Olean <gmolean7@yahoo.com>
2017-10-10 21:59:22 -0400

    cleaning up admin page. adding jquery and ajax to handle next and previous round button clicks

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -7,7 +7,6 @@ var http = require('http');
 const rounds = [10000, 5000, 2500, 2000, 1500, 1000, 500];
 var currRound = rounds[0];
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
     var totalDonations = 50000; // get total donations
     res.render('admin', { 
@@ -15,65 +14,30 @@ router.get('/', function(req, res, next) {
         rounds, 
         currRound, 
         totalDonations, 
-        currencyFormmatter,
-        nextRound: nextRound 
+        currencyFormmatter
     });
 });
 
 router.get('/currentRound', function(req,res, next){
-    res.send(currRound);
+    res.send({currRound});
 });
 
-router.get('/nextRound', function(req, res, next) {
+router.post('/nextRound', function(req, res, next) {
     var index = _.indexOf(rounds, currRound);
     if(index >= 0 && index < rounds.length - 1) {
         index++; //get next
         currRound = rounds[index];
     }
-    var totalDonations = 50000; // get total donations
-    res.render('admin', { 
-        title: 'Admin Page', 
-        rounds, 
-        currRound, 
-        totalDonations, 
-        currencyFormmatter,
-        nextRound 
-    });
+    res.send({currRound});
 });
 
-router.get('/prevRound', function(req, res, next) {
+router.post('/prevRound', function(req, res, next) {
     var index = _.indexOf(rounds, currRound);
-    console.log("currRound", currRound);
-    console.log('index', index);
     if(index > 0 && index <= rounds.length - 1) {
-        console.log('IN IF');
         index--; //get previous
         currRound = rounds[index];
     }
-    console.log("currRound", currRound);
-    var totalDonations = 50000; // get total donations
-    res.render('admin', { 
-        title: 'Admin Page', 
-        rounds, 
-        currRound, 
-        totalDonations, 
-        currencyFormmatter,
-        nextRound 
-    });
+    res.send({currRound});
 });
-
-function nextRound() {
-    console.log('in nextRound()!')
-    const options = {
-        hostname: 'http://localhost',
-        port: 3000,
-        path: '/admin/nextRound',
-        method: 'POST'
-    }
-    const req = http.request(options, (res) => {
-        // do nothing
-    });
-    req.end();
-}
 
 module.exports = router;
