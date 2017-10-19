ee3aa1d79aa8766b5889cbf8050468d8d3930133
562971 <georgeolean@C02V40ZDHTDG.local>
2017-10-18 22:47:02 -0400

    adding poll to get currentTotal

--- app/routes/clicker.js
+++ app/routes/clicker.js
@@ -2,14 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 var count = 0;
+var currentTotal = 13000;
+var round = 10000; // TODO: Get round from database
 
 router.get('/', function(req, res, next) {
-    var round = 1500; // TODO: Get round from admin service
     res.render('clicker', { round, count });
+});
+
+router.get('/currentTotal', function(req, res, next) {
+    res.send({currentTotal: currentTotal})
 });
 
 router.post('/addOne', function(req, res, next) {
     count++;
+    currentTotal = currentTotal + round;
     res.send({count});
 });
 
