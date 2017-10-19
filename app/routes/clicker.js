3973742582710ba747d6cd4041c207a909c6740e
562971 <georgeolean@C02V40ZDHTDG.local>
2017-10-18 22:52:35 -0400

    all files currently running on heroku

--- app/routes/clicker.js
+++ app/routes/clicker.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var accounting = require('../libs/service-accounting');
 
 var count = 0;
+var currentTotal = 13000;
+var round = accounting.getCurrentRound(); // TODO: Get round from database
 
 router.get('/', function(req, res, next) {
-    var round = 1500; // TODO: Get round from admin service
     res.render('clicker', { round, count });
+});
+
+router.get('/currentTotal', function(req, res, next) {
+    res.send({currentTotal: accounting.getCurrentTotal()})
 });
 
 router.post('/addOne', function(req, res, next) {
     count++;
+    accounting.increaseTotal();
     res.send({count});
 });
 
